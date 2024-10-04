ce5923770ed35260f9edf717d0a8c821616dbf08
Muhammad Aryan <160228293+MuhammadAriyan@users.noreply.github.com>
2024-10-04 19:06:41 +0500

    added files

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,100 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import React, { useState } from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Textarea } from "@/components/ui/textarea"
+
+interface DataObj {
+  name: string;
+  officialName?: string;
+  capital?: string;
+  population?: number;
+  continent?: string[];
+  flag?: string;
+}
+
+const CountryInfo = () => {
+  const [data, setData] = useState<null | DataObj>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+
+  async function fetchData(param1: string) {
+    try {
+      param1 = param1.trim().toLowerCase();
+      console.log(param1);
+
+      const res = await fetch(`https://restcountries.com/v3.1/name/${param1}`);
+
+      if (!res.ok) {
+        throw new Error("Invalid country");
+      }
+
+      const jsData = await res.json();
+
+      // If jsData is not valid, handle it with an error state
+      if (!jsData || jsData.status === 404) {
+        setError(true);
+        setData(null);
+        return;
+      }
+
+      let dataObj: DataObj = {
+        name: jsData[0]?.name.common,
+        officialName: jsData[0]?.name.official,
+        capital: jsData[0]?.capital,
+        population: jsData[0]?.population,
+        continent: jsData[0]?.continents,
+        flag: jsData[0]?.flags.png,
+      };
+
+      console.log(dataObj);
+      setData(dataObj);
+      setError(false);
+      setIsLoading(false);
+    } catch (e) {
+      console.log('Country not found');
+      setError(true);
+      setData(null);
+    }
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+    <div className="h-screen justify-center content-center p-1 font-mono font-bold">
+      <Card className="m-2 sm:m-1/6 md:m-1/4 lg:m-1/3 xl:m-1/2 p-3">
+        <CardHeader>
+          <CardTitle className="text-center">COUNTRINFO</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Textarea
+            placeholder="ENTER ANY COUNTRY"
+            className="text-center"
+            onChange={(event) => {
+              fetchData(event.target.value);
+            }}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </CardContent>
+        <CardDescription className="text-center">
+          {!isLoading && !error && data && (
+            <div className=' p-2 '>
+              <h1>NAME: {data?.name}</h1>
+              <h1>OFFICIAL NAME: {data?.officialName}</h1>
+              <h1>CAPITAL: {data?.capital}</h1>
+              <h1>CONTINENT: {data?.continent?.join(', ')}</h1>
+              <h1>POPULATION: {data?.population}</h1>
+              <img className='mx-auto rounded-lg m-2' src={data?.flag} alt={data?.officialName + " flag"} />
+            </div>
+          )}
+          {error && <p>No data found for the entered input.</p>}
+        </CardDescription>
+      </Card>
     </div>
   );
-}
+};
+
+export default CountryInfo;
